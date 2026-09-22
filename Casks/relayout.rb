9f9226af1b9c590f434f5f8ed98f8eb7ba428ea0
relayout-ci <ci@users.noreply.github.com>
2026-09-22 08:14:07 +0000

    relayout 1.3.3

--- Casks/relayout.rb
+++ Casks/relayout.rb
@@ -1,6 +1,6 @@
 cask "relayout" do
-  version "1.3.2"
-  sha256 "872085fffd550a9b8c4f6ce96a7d55956b8c992249ef7a48bc42358f317265a4"
+  version "1.3.3"
+  sha256 "c3b9affb449220197942e835294a877a385ca33ae0364e9e6e3d37ba72eee38c"
 
   url "https://github.com/vladforfutdinov/reLayout/releases/download/v#{version}/reLayout.dmg"
   name "reLayout"
